3b67c69d075e855dde39c8e43746412bed4331ed
EiadSS <eiads.sulaiman@gmail.com>
2024-03-23 00:49:19 -0400

    added cors config

---
+++ backend/src/main/java/com/eiadss/store/config/CorsConfig.java
@@ -0,0 +1,17 @@
+package com.eiadss.store.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://ecommerce-clothing-store-alpha.vercel.app/")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+    }
+}
