6110d4d3e97d69f07e9208bec203c6556becad31
EiadSS <eiads.sulaiman@gmail.com>
2024-03-23 15:42:49 -0400

    changed config

--- backend/src/main/java/com/eiadss/store/config/CorsConfig.java
+++ backend/src/main/java/com/eiadss/store/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://ecommerce-clothing-store-alpha.vercel.app/")
+                .allowedOrigins("https://ecommerce-clothing-store-alpha.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
